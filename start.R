63491f08af82a4cdf96140775826854840fa91cb
André Calero Valdez <andrecalerovaldez@gmail.com>
2023-01-16 23:06:31 +0100

    initial commit

---
+++ start.R
@@ -0,0 +1,23 @@
+# This is a comment. Comments in R start with a hash Tag!
+
+# seminr contains the data
+library(seminr)
+
+library(esquisse)
+# shows us a help about the data set
+?seminr::mobi
+
+# make the data available in the global environment
+mobi <- seminr::mobi
+
+
+# Quartiles
+summary(mobi$CUEX1)
+
+# descriptives
+mean(mobi$CUEX1)
+median(mobi$CUEX1)
+sd(mobi$CUEX1)
+
+t.test(mobi$CUEX1, conf.level = .95)
+
